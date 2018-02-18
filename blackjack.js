68dff5e83e5ee40aa41eff8c083c43f7251a14d6
Pavel Sidorenko <pavel_sidorenko@epam.com>
2018-02-18 16:38:42 +0300

    fixes

--- blackjack.js
+++ blackjack.js
@@ -2,9 +2,9 @@ var readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 });
-
-var faces = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-var suites = ['Hearts', 'Spades', 'Diamonds', 'Clubs'];
+var fs = require('fs');
+var faces = [ 'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K' ];
+var suites = [ 'Hearts', 'Spades', 'Diamonds', 'Clubs' ];
 var hand = new Hand();
 
 function Card(s, n) {
@@ -17,19 +17,21 @@ function Card(s, n) {
         return suit;
     };
     this.getValue = function () {
-        if (number > 10) {
-            return 10;
-        } else if (number === 1) {
-            return 11;
-        } else {
-            return number;
-        }
+      if (number == 'A') {
+        return 11;
+      } else if (number == 'J' || number == 'Q' || number == 'K') {
+        return 10;
+      } else {
+        return number;
+      }
     };
 }
 
 function deal() {
-    var randSuit = Math.floor(Math.random() * 4) + 1;
-    var randNumber = Math.floor(Math.random() * 13) + 1;
+    var randSuit = suites[Math.floor(Math.random() * 4)];
+    var randFaces = Math.floor(Math.random() * faces.length)
+    var randNumber = faces[randFaces];
+    faces.splice(randFaces, 1);
     return new Card(randSuit, randNumber);
 }
 
@@ -40,14 +42,14 @@ function Hand() {
     this.score = function () {
         var sum = 0;
         for (var i = 0; i < cards.length; i++) {
-            sum += cards[i].getValue();
+            sum += +cards[i].getValue();
         }
         return sum;
     };
     this.printHand = function () {
         var string = "";
         for (var i = 0, length = cards.length; i < length; i++) {
-            string += cards[i].getNumber() + " of suit " + cards[i].getSuit();
+            string += cards[i].getNumber() + " of " + cards[i].getSuit();
             string += (i === length - 1) ? "" : ", ";
         }
         return string;
@@ -58,21 +60,21 @@ function Hand() {
 }
 
 function playGame() {
-    console.log("Your hand is " + hand.printHand());
-    console.log("Your score is " + hand.score());
+  console.log("Your hand is " + hand.printHand());
+  console.log("Your score is " + hand.score());
     if (hand.score() > 21) {
-        console.log("You lose!");
-        readline.close();
+      readline.close();
+      console.log("You lose!");
     } else {
-        readline.question("[H]it or [S]tay? ", function (answer) {
-            if (answer == 'h' || answer == 'H') {
-                hand.hitMe();
-                playGame();
-            } else if (answer == 's' || answer == 'S') {
-                readline.close();
-                console.log("Your final score is " + hand.score());
-            }
-        });
+      readline.question("[H]it or [S]tay? ", function (answer) {
+          if (answer == 'h' || answer == 'H') {
+              hand.hitMe();
+              playGame();
+          } else if (answer == 's' || answer == 'S') {
+              readline.close();
+              console.log("You win!");
+          }
+      });
     }
 }
 
